a3a0cd2cee1be2c14944a543b1a1f8a2c541a465
Sébastien Lejeune <seb.lejeune.web@gmail.com>
2024-09-15 22:36:33 +0200

    Add domainé

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://5ipid.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/cours-documentation/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
