3af6f9e2589a860b6738278be33f22425558c0ab
Sébastien Lejeune <seb.lejeune.web@gmail.com>
2024-09-15 17:58:35 +0200

    add automatic deploy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -88,7 +88,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/5IPID/cours-documentation',
+          href: 'https://github.com/5IPID/cours-documentation.git',
           label: 'GitHub',
           position: 'right',
         },
